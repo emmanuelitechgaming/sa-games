1a6e4de84a740f0b59fc75b15c736e7ae053c4ce
emmanuelitechgaming <emmanuel.ramos@itechgaming.com>
2017-03-25 15:43:35 +0800

    added ,modified files

--- sa-games/js/main.js
+++ sa-games/js/main.js
@@ -1,1 +1,17 @@
+$(document).ready(function() {
 
+  $( ".arrow" ).click(function() {
+
+  if ($('.footerContainer').hasClass('open')) {
+      $('.footerContainer').removeClass('open');
+      $('.slide').removeClass('open');
+
+  } else {
+      $('.footerContainer').addClass('open');
+      $('.slide').addClass('open');
+  }
+
+  });
+
+
+});
